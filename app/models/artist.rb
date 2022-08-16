ea80e0841ac548fba6f9e496b6c4ed3d279c2d57
Jonathon <jwbialas@gmail.com>
2022-08-16 14:55:47 -0500

    fin

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,10 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+    Artist.column_names
+    a = Artist.new(name: 'Jon')
+    a.age = 30
+    a.save
+    Artist.create(name: 'Kelly')
+    Artist.all
+    Artist.find_by(name: 'Jon')
+end
