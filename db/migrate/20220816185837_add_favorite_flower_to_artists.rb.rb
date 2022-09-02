5d73201b120eebf6de7d0b31095168e542a2b578
Jonathon <jwbialas@gmail.com>
2022-09-02 17:16:29 -0500

    fin

--- db/migrate/20220816185837_add_favorite_flower_to_artists.rb.rb
+++ db/migrate/20220816185837_add_favorite_flower_to_artists.rb.rb
@@ -1,5 +1,6 @@
 class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
   def change
-    add_column :artists, :favorite_flower, :string
+      add_column :artists, :favorite_flower, :string 
+    end
   end
 end
